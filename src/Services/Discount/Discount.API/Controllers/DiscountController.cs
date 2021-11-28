86f90a2fdb4c66c4eefb1a2c00b8afc27d1134ae
Joe <jmummdbd@att.net>
2021-11-28 11:40:23 -0800

    Added Npgsql and Dapper and Updated Swagger Packages. Added Repository classes for Discount.API
    Section 52

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Controllers
+{
+  [ApiController]
+  [Route("api/v1/[controller]")]
+  public class DiscountController : ControllerBase
+  {
+    private readonly ILogger<DiscountController> _logger;
+
+    public DiscountController(ILogger<DiscountController> logger)
+    {
+      _logger = logger;
+    }
+
+    //[HttpGet]
+  }
+}
