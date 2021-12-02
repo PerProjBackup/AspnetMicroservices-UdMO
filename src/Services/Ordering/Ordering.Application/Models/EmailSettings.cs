f49435dcc8b5f072483b3614159192dcd3cfee2b
Joe <jmummdbd@att.net>
2021-12-02 15:51:25 -0800

    Created Ordering.Applications Folders Behaviours, Contracts, Features, Mapings and Models. Under Contracts Created infrastrucutre and Persistence Folders. Added classes to all but Features. Installed AutoMapper Package..
    Section 91-92

---
+++ src/Services/Ordering/Ordering.Application/Models/EmailSettings.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Ordering.Application.Models
+{
+  public class EmailSettings
+  {
+    public string TApiKeyo { get; set; }
+    public string FromAdress { get; set; }
+    public string FromName { get; set; }
+
+  }
+}
