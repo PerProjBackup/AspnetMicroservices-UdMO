316e6fe7714e95e21a9413ba7634c443b308eea7
Joe <jmummdbd@att.net>
2021-12-09 11:14:10 -0800

    Added and Tested a Checkout Action in the Basket.API to Produce and Publish an EventMessage to  RabbitMQ EventBus
    Section 120-122

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -1,6 +1,7 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -27,17 +28,24 @@ namespace Basket.API
         options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
       });
 
+      services.AddScoped<IBasketRepository, BasketRepository>();
+      services.AddAutoMapper(typeof(Startup));
+
+      services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>( o =>
+        o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));      
+      services.AddScoped<DiscountGrpcService>();
+
+      services.AddMassTransit(config => {
+        config.UsingRabbitMq((ctx, cfg) => { cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+        });
+      });
+      services.AddMassTransitHostedService();
+
       services.AddControllers();
       services.AddSwaggerGen(c =>
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
       });
-
-      services.AddScoped<IBasketRepository, BasketRepository>();
-
-      services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>( o =>
-        o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
-      services.AddScoped<DiscountGrpcService>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
